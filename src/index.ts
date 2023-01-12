12a83aa35dd6cc9f7de446cfaf489cfb2ebe17bb
Basanta Sapkota <sapkota.basanta46@gmail.com>
2023-01-13 00:03:20 +0545

    Added Express Server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("Hello World");
+import express from 'express';
+import morgan from 'morgan';
+
+// Define an express server application
+const app = express();
+
+// Use morgan as default logger for server
+app.use(morgan('dev'));
+
+// Demo RESTAPI endpoint setup
+app.get('/', (req, res) => {
+  res.json({
+    hello: 'world',
+  });
+});
+
+// Start the server & listen on specified port
+const port = Number(process.env.PORT ?? 8080);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
