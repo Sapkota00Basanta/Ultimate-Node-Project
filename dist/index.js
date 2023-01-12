a6df44364487630e32d29f3cf8619d40087fb0b5
Basanta Sapkota <sapkota.basanta46@gmail.com>
2023-01-12 23:43:36 +0545

    Removing dist directory

--- dist/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-console.log("Hello World");
-
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL3NyYy9pbmRleC50cyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZyhcIkhlbGxvIFdvcmxkXCIpO1xuIl0sIm5hbWVzIjpbImNvbnNvbGUiLCJsb2ciXSwibWFwcGluZ3MiOiI7QUFBQUEsUUFBUUMsR0FBRyxDQUFDIn0=
